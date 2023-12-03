241f1d43d75075b354b700de2b55ba50c8bcce0b
Yaniv Ben Hemo <Yaniv@memphis.dev>
2023-12-03 08:59:56 +0200

    update

--- split-with-delimiter/index.js
+++ split-with-delimiter/index.js
@@ -12,21 +12,14 @@ exports.handler = async (event) => { // The name of this file and this function 
  * @returns {Object} 
  */
 function eventHandler(payload, headers, inputs) {
-    // Handle event here
-    
     // Here is a short example of converting the payload to a json object and returning it as an Uint8Array
-    // const decodedPayload = payload.toString('utf-8');
-    
-    // const asJson = JSON.parse(payload);
-    const asJson = payload
-    // const field = inputs.field_to_split.split(".")
-    // console.log(field)
-    // console.log(asJson[field[0]]["object"])
-    // console.log(inputs2)
-    // console.log(asJson[inputs2])
-    asJson[inputs.key_to_split]=asJson[inputs.key_to_split].split(inputs.delimiter)
+    const decodedPayload = payload.toString('utf-8');
+    const asJson = JSON.parse(payload);
 
-    // asJson[inputs.field_to_split]=asJson[inputs.field_to_split].split(inputs.delimiter)
+    // START Handle event here
+    asJson[inputs.key_to_split]=asJson[inputs.key_to_split].split(inputs.delimiter)
+    
+    // END Handle event here
 
     return {
         processedMessage: Buffer.from(JSON.stringify(asJson), 'utf-8'),
